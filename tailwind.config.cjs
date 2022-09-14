c7697ef50a75ef1d5936c9a3a8493db016229b3a
Sri Harsh Amur <sri@appsfortableau.infotopics.com>
2022-09-15 01:47:56 +0200

    Added Hero and About Section with responsiveness

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryLight: "#F8F8F8",
+        secondaryLight: "#212121",
+        tertiaryLight: "#EE5355",
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
